796168cb854d64676f1d97c534082799e8de9d5a
cjezierski <corentin.jezierski@gmail.com>
2024-10-03 12:27:42 +0200

    add darkMode system

--- front/src/Component/App/App.jsx
+++ front/src/Component/App/App.jsx
@@ -1,25 +1,27 @@
-import { useDispatch, useSelector } from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import './App.css';
 import {decrement, increment, login, register} from '../../Redux/userSlice';
-import { useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {useEffect} from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from "../Home/Home";
 import Register from "../Register/Register";
 import Login from "../Login/Login";
+import {ThemeProvider} from "../../Context/ThemeContext/ThemeProvider";
 
 function App() {
 
 
-
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/register" element={<Register />} />
-				<Route path="/login" element={<Login />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- front/src/Component/Home/Home.jsx
+++ front/src/Component/Home/Home.jsx
@@ -1,13 +1,15 @@
-import {useEffect} from "react";
+import {useContext, useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {isConnected, disconnected, login} from "../../Redux/userSlice";
 import {useNavigate} from "react-router-dom";
+import {ThemeContext} from "../../Context/ThemeContext/ThemeProvider";
 
 function Home() {
 
   const dispatch = useDispatch();
   let navigate = useNavigate()
   const {connected} = useSelector(state => state.user);
+  const {changeTheme} = useContext(ThemeContext);
 
 
   useEffect(() => {
@@ -19,6 +21,7 @@ function Home() {
   return (
     <>
       <div>Home</div>
+      <button onClick={()=>changeTheme()} >Change mode</button>
       <button onClick={() => dispatch(disconnected())}>Déconnexion</button>
     </>
 
